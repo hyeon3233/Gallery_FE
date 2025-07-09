d154d3cf2f85a50118f33b58aef2459fe7b269dc
hyeon3233 <yeonho3233@naver.com>
2025-07-09 16:49:01 +0900

    cart 작업

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/join',
+      component: () => import('@/views/Join.vue'),
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/Login.vue'),
+    },
+    {
+      path: '/cart',
+      component: () => import('@/views/Cart.vue')
+    }
+  ],
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { getItems } from '@/services/itemService';
+import { onMounted, reactive } from 'vue';
+import Card from '@/components/Card.vue';
+
+const state = reactive({
+  items: [],
+});
+
+onMounted(async () => {
+  const res = await getItems();
+  if (res.status !== 200) {
+    return;
+  }
+  console.log('res.data: ', res.data);
+  state.items = res.data;
+});
+</script>
+
+<template>
+  <div class="home">
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-3">
+          <div class="col" v-for="item in state.items">
+            <Card :item="item" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
